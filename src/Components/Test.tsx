2dfa61983e25326b9b65a5717c630343b3f33756
Ido Mandelman <idomand@gmail.com>
2022-05-02 08:55:59 +0200

    test

---
+++ src/Components/Test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./TestStyle.module.css";
+type Props = {};
+
+export default function Test({}: Props) {
+  return <div className={styles.poop}>Test111</div>;
+}
