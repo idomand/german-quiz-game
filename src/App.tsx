5ceb0204b408a3d3dfe123137ef53d3d9379ca37
Ido Mandelman <idomand@gmail.com>
2022-05-26 13:28:54 +0200

    changed meta data

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import DarkMoodButton from "./Components/DarkMoodButton";
 export default function App() {
   const { themeColor } = useTheme();
 
-  console.log("themeColor :>> ", themeColor);
-
   return (
     <main className={`app ${themeColor}`}>
       <DarkMoodButton />
