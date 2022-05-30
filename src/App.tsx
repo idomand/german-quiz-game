40b86227e2766d02aa6ee9bfd186aa21d8b2c3d6
Ido Mandelman <idomand@gmail.com>
2022-05-30 13:53:20 +0200

    big cahnges

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ArtikelGame from "./Components/ArtikelGame";
 import "./App.scss";
 import { useTheme } from "./Util/ThemeContext";
 import DarkMoodButton from "./Components/DarkMoodButton";
 
 export default function App() {
+  const [gameType, setGameType] = useState<string | null>("artikel");
   const { themeColor } = useTheme();
 
   return (
     <main className={`app ${themeColor}`}>
-      <DarkMoodButton />
-      <ArtikelGame />
+      <header>
+        <h1>German Word Games</h1>
+        <DarkMoodButton />
+      </header>
+      <div className="gamePickerWrapper">
+        <h2>What game would you like to play?</h2>
+        <button
+          className="appButton"
+          onClick={() => {
+            setGameType("artikel");
+          }}
+        >
+          Artikel Game
+        </button>
+        <button
+          className="appButton"
+          onClick={() => {
+            setGameType("time");
+          }}
+        >
+          Time Game
+        </button>
+      </div>
+
+      {gameType === "artikel" && <ArtikelGame />}
+      {gameType === "time" && <p>time game</p>}
     </main>
   );
 }
